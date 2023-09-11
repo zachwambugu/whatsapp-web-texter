122978bffb8bdc384a5a2b8058c3dac05f3dd045
zachwambugu <118126753+zachwambugu@users.noreply.github.com>
2023-09-11 10:58:47 +0300

    Add files via upload

---
+++ whatsapp web texter.py
@@ -0,0 +1,14 @@
+import random
+import pyautogui as pg
+import time
+
+#open whatsapp web and the contact to send the messages
+# run this code
+#go to whatsapp web and it will send messages according to the instructions
+
+#time below is a delay from when you run this code to when it will start sending the messages
+time.sleep(8)
+for i in range(500):
+    pg.write("Githui")
+    pg.press('enter')
+    
